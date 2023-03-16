8fedf640f1a83e9884974ac24d857849df59fae3
markphillippebworth <markphillip.pebworth@alleninstitute.org>
2023-03-16 17:44:34 +0000

    CTCF processing scripts.

--- Fig2/.ipynb_checkpoints/processCTCF-checkpoint.R
+++ Fig2/.ipynb_checkpoints/processCTCF-checkpoint.R
@@ -1,4 +1,4 @@
-
+library(plyranges)
 
 ## Download CTCF ChIP-seq peak calls from all Blood cell types. 
 #https://chip-atlas.org/peak_browser
@@ -6,4 +6,8 @@
 #Unfortunately, they download with the same name, so I labeled the Hg19 one as Hg19. 
 
 
-Hg38 <-
+Hg38 <- plyranges::read_bed('Oth.Bld.05.CTCF.AllCell.bed')
+plyranges::reduce_ranges(Hg38) %>% plyranges::write_bed('All_Blood_CTCF.bed')
+
+Hg19 <- plyranges::read_bed('Oth.Bld.05.CTCF.AllCell_Hg19.bed')
+plyranges::reduce_ranges(Hg19) %>% plyranges::write_bed('All_Blood_CTCF_hg19.bed')
